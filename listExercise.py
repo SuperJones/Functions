67a3bf770afc46027d2eebf8c6ab896f6ed0f1f5
Anissa Jones <anissajones10@gmail.com>
2015-06-22 21:13:42 -0400

    practice by doing.

--- listExercise.py
+++ listExercise.py
@@ -4,3 +4,21 @@ print(myList)
 
 myList = myList + [76]
 print(myList)
+
+#Append “apple” and 76 to the list.
+myList.append("apple")
+myList.append(76)
+print(myList)
+
+#Insert the value “cat” at position 3.
+myList.insert(3, "cat")
+print(myList)
+
+#Insert the value 99 at the start of the list.
+myList.insert(0, 99)
+print(myList)
+
+#Find the index of “hello”.
+print(myList.index("hello"))
+
+
