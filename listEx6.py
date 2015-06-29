fe5e05db76bb18166c5f7259e99e2ed487b1964c
Anissa Jones <anissajones10@gmail.com>
2015-06-29 19:33:09 -0400

    There was an error in space/indentation before. Fixed that. Added second for loop to add numbers. Added instructions for other peoples benefit.

--- listEx6.py
+++ listEx6.py
@@ -1,9 +1,16 @@
-
 def sum_of_squares(xs):
-	theSquare = []
-	for j in xs:
-	    new_list = j ** 2
-		theSquare.append(new_list)
-	return theSquare
+    theSquare = []
+    for j in xs:
+        new_list = j ** 2
+        theSquare.append(new_list)
+    finalNum = 0
+    for i in theSquare:
+	    finalNum = finalNum + i 
+    return finalNum
 
 print(sum_of_squares([2,3,4]))
+
+#Write a function sum_of_squares(xs) that computes the sum of 
+#the squares of the numbers in the list xs. 
+#For example, sum_of_squares([2, 3, 4]) 
+#should return 4+9+16 which is 29.
