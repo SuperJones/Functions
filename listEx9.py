1cdb91022afebcdf518d8319ee9b9376053e3d30
Anissa Jones <anissajones10@gmail.com>
2015-06-29 20:22:05 -0400

    added listEx9.py. accidently  editted Ex7 but changed it back. in example 9 i only had to change the 1 in the if statement to an i.

--- listEx9.py
+++ listEx9.py
@@ -4,7 +4,7 @@ def oddNum(yourList):
         if i % 2 == 0:
             counter = counter + 0
         else:
-            counter = counter + 1
+            counter = counter + i
     return counter
 
 print (oddNum ([1,2,3,4,5,6,7,8,9,10,11]))
