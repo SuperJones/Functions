ff63b99f3a16ecddb0131871f50c11d6a4d9b5ad
Anissa Jones <anissajones10@gmail.com>
2015-06-29 20:15:10 -0400

    start off with the code from listEx7.py

---
+++ listEx9.py
@@ -0,0 +1,11 @@
+def oddNum(yourList):
+    counter = 0
+    for i in yourList:
+        if i % 2 == 0:
+            counter = counter + 0
+        else:
+            counter = counter + 1
+    return counter
+
+print (oddNum ([1,2,3,4,5,6,7,8,9,10,11]))
+
