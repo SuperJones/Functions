45c135fadc48303160fe2d083c0b8d19efcdebb7
Anissa Jones <anissajones10@gmail.com>
2015-07-01 16:52:29 -0400

    Went back through book and rediscovered the split and join method. I just have to figure out to use them so they work for this function.

--- listEx14.py
+++ listEx14.py
@@ -10,4 +10,8 @@ def replace(s, old, new):
     return newString       
 
 stringMaybe = 'I love spom!  Spom, yum!'
-print(replace(stringMaybe, 'o', 'a'))
+print(replace(stringMaybe, 'om', 'am'))
+
+#went back through reading and rediscovered the split and join method.
+#so I have to figure out how to split it so that "om" comes out.
+# and then join it with "am" in its place.
