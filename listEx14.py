6b06aa12ed3aa1befff08c96d03a126f7949b2de
Anissa Jones <anissajones10@gmail.com>
2015-07-01 16:55:02 -0400

    Playing aroung with the split method. It takes out the om but that is it.

--- listEx14.py
+++ listEx14.py
@@ -1,17 +1,7 @@
 def replace(s, old, new):
-    newString = ''
-    for i in s:
-        if i == old:
-            #change it
-            newString = newString + new
-        else:    
-            #leave it
-            newString = newString + i
-    return newString       
+    tempString = s.split(old)
+    return tempString       
 
 stringMaybe = 'I love spom!  Spom, yum!'
 print(replace(stringMaybe, 'om', 'am'))
 
-#went back through reading and rediscovered the split and join method.
-#so I have to figure out how to split it so that "om" comes out.
-# and then join it with "am" in its place.
