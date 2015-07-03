db7370e02af4489be1aa4d6f4fadd35825adf0b6
Anissa Jones <anissajones10@gmail.com>
2015-07-03 13:10:06 -0400

    I almost have it, but i have to figure out how to turn the values into an int without disturbing the name value.

--- filesEx2.py
+++ filesEx2.py
@@ -10,22 +10,11 @@ def average(yourList):
 	for i in yourList:
         theSum = theSum + i
 	return theSum
-	
-#first I need the to separate the numbers and turn them into ints
-def separate (your_file):
-	values = your_file.split()
-	finalNum = 0
-	for anumber in values:
-	    if anumber != values[0] and anumber < len(values):
-	    	anumber = int(anumber)
-	    	finalNum = finalNum + anumber
-	    else:
-	    	finalNum = finalNum + 0 
-	return finalNum
 
-print(separate(line))
-#then I need to be able to add them and divide by the number of items
-#then I need to be able to print the a returned value next to the name
+
+while line:
+	values = line.split()
+	print (values[0], "average is", average(values[1:]))
 
     
 infile.close()
