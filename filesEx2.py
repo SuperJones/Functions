6d43c97596cdff2d3170422873259817ca37e2b0
Anissa Jones <anissajones10@gmail.com>
2015-07-03 11:34:49 -0400

    first try. it prints out unexpected outcome of 0.

--- filesEx2.py
+++ filesEx2.py
@@ -1,10 +1,26 @@
+#write a program that calculates the average grade for each student, 
+#and print out the student’s name along with their average grade.
+
+
 infile = open("C:\Users\NinjaZeroOne\Desktop\studentdata.txt", "r")
 line = infile.readline()
 
-while line:
-    values = line.split()
-    if this:
-        print(this)
-    line = infile.readline()
+
+#first I need the to separate the numbers and turn them into ints
+def separate (your_file):
+	values = your_file.split()
+	finalNum = 0
+	for anumber in values:
+	    if anumber != values[0] and anumber < len(values):
+	    	anumber = int(anumber)
+	    	finalNum = finalNum + anumber
+	    else:
+	    	finalNum = finalNum + 0 
+	return finalNum
+
+print(separate(line))
+#then I need to be able to add them and divide by the number of items
+#then I need to be able to print the a returned value next to the name
+
     
 infile.close()
