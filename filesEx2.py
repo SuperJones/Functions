d007164299aae8c05b52f31e30cf5eba914a87ae
Anissa Jones <anissajones10@gmail.com>
2015-07-03 10:05:12 -0400

    starter or pseudo code for exercise 2.

---
+++ filesEx2.py
@@ -0,0 +1,10 @@
+infile = open("C:\Users\NinjaZeroOne\Desktop\studentdata.txt", "r")
+line = infile.readline()
+
+while line:
+    values = line.split()
+    if this:
+        print(this)
+    line = infile.readline()
+    
+infile.close()
