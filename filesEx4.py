aba1496ea6df9e2bc9164470c9e73b054182af71
Anissa Jones <anissajones10@gmail.com>
2015-07-03 19:52:01 -0400

    made a function to add the x or y coordinates together.

--- filesEx4.py
+++ filesEx4.py
@@ -1,5 +1,6 @@
-f = open("C:\Users\NinjaZeroOne\Desktop\httlacslabdata.txt", 'r')
-line = f.readline()
+import turtle
+
+labfile = open("C:\Users\NinjaZeroOne\Desktop\httlacs\labdata.txt", 'r')
 
 
 def average(yourList):
@@ -10,6 +11,14 @@ def average(yourList):
 	finalNum = theSum / (len(yourList) -1)
     return finalNum
 
+def sumthem(yourFile, x_or_y):
+    newline = yourFile.readline()
+    add_them = 0
+    while newline:
+        values = newline.split()
+        add_them = add_them + (int(values[x_or_y]))
+        newline = yourFile.readline()
+    return add_them
 
 def m_formula(numPoints)
 	m = (the sum of x times the sum of y) - ( n times mean of x values times mean of y values)
@@ -19,13 +28,32 @@ def m_formula(numPoints)
 def plotRegression(line):
 	reads the data from the file and uses a turtle to plot points
 
+#n equals the number of points which is the number of lines in the file 
+linelist = f.readlines()
+n = (len(linelist) + 1)
 
-n = the number of points which is the number of lines in the file 
+x_sum = sumthem(labfile, 0)
+y_sum = sumthem(labfile, 1)
 
 y = (the sum of y values) + (m_formula(n)) times (x - (sum of x values))
 
+
+
+
+
+xs = [48, 117, 200, 240, 160, 260, 220]  # here is the data
+maxheight = max(xs)
+numbars = len(xs)
+border = 10
+
+wn = turtle.Screen()
+wn.setworldcoordinates(0-border, 0-border, 40*numbars+10, maxheight+10)
+#Make the window fit the data
 
 while line:
     values = line.split()
     print (values[0], "average is", average(values[1:]))
     line = infile.readline()
+
+f.close()
+wn.exitonclick
