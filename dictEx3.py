d00b6e450d5dd92e146664f79e1c91b0cb65ae08
Anissa Jones <anissajones10@gmail.com>
2015-07-11 16:50:59 -0400

    So I think I have the basic program written but I don't think I have it being written to another file. More research is required.

--- dictEx3.py
+++ dictEx3.py
@@ -8,20 +8,15 @@ aliceDiction = {}           #create empty dictionary
 for aline in infile:
     values = aline.split()  #split each line into words
     for aword in values:    #check to see if word is in dictionary
-        if aword in
- 
-for achar in userString:
-    if achar in string.ascii_lowercase:
-        if achar in stringDiction:
-            stringDiction[achar] = stringDiction[achar] + 1
-        else:   
-            stringDiction[achar] = 1
+        if aword in aliceDiction:
+            aliceDiction[aword] = aliceDiction[aword] + 1
+        else:
+            aliceDiction[aword] = 1
  
    
-keys = stringDiction.keys()
+keys = aliceDiction.keys()
 keys.sort()
-for achar in keys:
-    print(achar, stringDiction[achar])
+ 
  
  
 infile.close()
