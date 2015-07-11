6a0667f3a932bb5a72042262589077b2e08457ae
Anissa Jones <anissajones10@gmail.com>
2015-07-11 16:31:36 -0400

    this exercise was easy. It came with the test so, I just had to create the function and make sure the test came out correctly.

---
+++ dictEx2.py
@@ -0,0 +1,19 @@
+def add_fruit(inventory, fruit, quantity = 0):
+     if fruit in inventory:
+        inventory[fruit] = inventory[fruit] + quantity
+     else:
+        inventory[fruit] = quantity
+ 
+# make these tests work...
+new_inventory = {}
+# add_fruit(new_inventory, 'strawberries', 10)
+# test('strawberries' in new_inventory, True)
+# test(new_inventory['strawberries'], 10)
+# add_fruit(new_inventory, 'strawberries', 25)
+# test(new_inventory['strawberries'] , 35)
+ 
+add_fruit(new_inventory, 'strawberries', 10)
+print ('strawberries' in new_inventory)
+print (new_inventory)
+add_fruit(new_inventory, 'strawberries', 25)
+print(new_inventory['strawberries'])     
