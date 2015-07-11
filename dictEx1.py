422d4b5279bb431c2b842ae0ba005634ce3c6377
Anissa Jones <anissajones10@gmail.com>
2015-07-11 15:58:58 -0400

    still thinking thing through.

--- dictEx1.py
+++ dictEx1.py
@@ -1,12 +1,15 @@
-#Write a program that allows the user to enter a string. It then prints a #table of the letters of the alphabet in alphabetical order which occur in #the string together with the number of times each letter occurs. Case #should be ignored. A sample run of the program might look this this:
+#Write a program that allows the user to enter a string. 
+#It then prints a table of the letters of the alphabet in 
+#alphabetical order which occur in #the string together with the 
+#number of times each letter occurs. Case should be ignored. 
 
 
+
+import string
+ 
 userString = (raw_input("Please enter a sentence: "))
  
 for achar in userString:
-    alpha_count = {}
-    compare_alpha = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    if achar in compare_alpha:
-        if achar.isalpha:
-        else:
+    if achar == string.ascii_lowercase or achar == string.ascii_uppercase:
+        print("yes")
     else:
