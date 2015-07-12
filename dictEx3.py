d1651ad6b08b963ef5543d6a07311bf76e2593b6
Anissa Jones <anissajones10@gmail.com>
2015-07-11 20:12:37 -0400

    So the previous version did not work. I got an error about the concatenation at line 22. I went back and tried to block out were the issue was. I think the issue happened when I tried to use the lower method. I erased it and now I see that I need to take out punctuation because it is an issue in the dictionary.

--- dictEx3.py
+++ dictEx3.py
@@ -1,23 +1,28 @@
 import string
  
-infile = open("C:\Users\ZeroNinjaOne\Desktop\httlacs\AliceInWonderland.txt", 'r')
-outfile = open("alice_words.txt", 'w')
+infile = open("C:\Users\NinjaZeroOne\Desktop\httlacs\AliceInWonderland.txt", 'r')
+#outfile = open("alice_words.txt", 'w')
  
 aliceDiction = {}           #create empty dictionary
  
 for aline in infile:
     values = aline.split()  #split each line into words
     for aword in values:    #check to see if word is in dictionary
+       
         if aword in aliceDiction:
             aliceDiction[aword] = aliceDiction[aword] + 1
         else:
             aliceDiction[aword] = 1
  
-   
-keys = aliceDiction.keys()
-keys.sort()
- 
- 
- 
-infile.close()
-outfile.close()
+  
+print(aliceDiction)   
+#keys = aliceDiction.keys()
+#keys.sort()
+
+#for aword in keys:
+	#print(str(aword) + "" + str(aliceDiction[aword]))
+    #outfile.write(aword + "" + str(aliceDiction[aword]) )
+    #outfile.write('\n')
+
+print("The word 'alice' appears " + str(aliceDiction['alice']) + "times in the book.")    
+#outfile.close()
