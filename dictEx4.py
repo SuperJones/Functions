cfad25ec9860ed2e34e5606c657c3468c8a53860
Anissa Jones <anissajones10@gmail.com>
2015-07-12 00:02:21 -0400

    Took the part of exercise 3 that made the dictionary and added the last part that tested wether the key was longest or not. Apparently in alice and wonderland the longest word is importantunimportunimportantimportant.

--- dictEx4.py
+++ dictEx4.py
@@ -4,23 +4,39 @@ infile = open("C:\Users\NinjaZeroOne\Desktop\httlacs\AliceInWonderland.txt", 'r'
  
 aliceDiction = {}   #create empty dictionary
  
-for aline in infile:
-    values = aline.split()  #split each line into words
-    for aword in values:    #check to see if word is in dictionary
-        if aword in aliceDiction:
-            aliceDiction[aword] = aliceDiction[aword] + 1
-        else:
-            aliceDiction[aword] = 1
-           
-j = 0           
-for akey in aliceDiction.keys:
+aliceDiction = {}           #create empty dictionary
+ 
+for aline in infile:  
+    for aword in aline.split():        
+        
+        # remove punctuation
+        aword = aword.replace('_', '').replace('"', '').replace(',', '').replace('.', '')
+        aword = aword.replace('-', '').replace('?', '').replace('!', '').replace("'", "")
+        aword = aword.replace('(', '').replace(')', '').replace(':', '').replace('[', '')
+        aword = aword.replace(']', '').replace(';', '')
+        
+        aword = aword.lower()
+
+        if aword.isalpha():             #Make sure it is not a number
+            if aword in aliceDiction:  #Check to see if word is in dictionary
+                aliceDiction[aword] = aliceDiction[aword] + 1
+            else:
+                aliceDiction[aword] = 1
+ 
+    
+keys = aliceDiction.keys()
+keys.sort()
+
+ks = list(aliceDiction.keys())           
+j = 0   
+largest_word = 0        
+for akey in ks:
     if len(akey) >= j:
         largest_word = akey
         j = len(akey)           
- 
    
  
  
-print(aliceDiction[alice])
+print(largest_word)
  
 infile.close()
