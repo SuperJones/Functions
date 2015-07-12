4aace9d3db054231df8089e059a2cf55df47a13e
Anissa Jones <anissajones10@gmail.com>
2015-07-12 00:18:32 -0400

    The part from exercise 4 just gave me the longest word, but I need to be able to replace the words from a users sentence with the matching word in the dictionary. I changed it to fix that. The code now says that like 16 is not iterable. So I think that a part from exercise 4 will work because I had to fix that issue there as well.

--- dictEx5.py
+++ dictEx5.py
@@ -1,7 +1,7 @@
 pirate_dict = {'man': 'matey', 'is':'b', 'hello':'avast', 
                'my':'me', 'restroom':'head', 'the':"th'", 
               'lawyer':'foul blaggart', 'are':'be', 
-               'students':'swabbies', 'excuse':'arr' 'your':'yer',
+               'students':'swabbies', 'excuse':'arr', 'your':'yer',
                'restaurant':'galley', 'professor':'foul blaggart', 
                'madam':'proud beauty', 'boy':'matey', 'student':'swabbie',
                'hotel':'fleabag inn', 'sir':'matey'}
@@ -9,12 +9,13 @@ pirate_dict = {'man': 'matey', 'is':'b', 'hello':'avast',
 #Or I could create a file and create the dictionary based
 #off the file.
 
-ks = list(pirate_dict.keys())
-j = 0
-largest_word = 0
-for akey in ks:
-    if len(akey) >= j:
-        largest_word = akey
-        j = len(akey)            
-
-print(largest_word)
+user_string = raw_input("Type in regular english sentence: ")
+ 
+pirate_string = ""
+for aword in user_string:
+    for akey in pirate_dict.keys:
+        if aword == akey:
+            user_string[aword] = akey
+                  
+ 
+print(user_string)
