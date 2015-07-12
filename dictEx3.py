2fb1ef0f51e43dcc5c5b0eb89bfd919e0ad175a9
Anissa Jones <anissajones10@gmail.com>
2015-07-11 23:19:52 -0400

    so it looks like it works. I tried to find a better way to take out punctutation online but to no avail. There were other ways, but they didn't seem better. I also was having indentation errors for some reason. I erased the lines and retyped them to fix it. I understand that I was going in the right direction but I was missing some basic understandings of writing files and how the punctutation and case sensitivity would effect the outcome.

--- dictEx3.py
+++ dictEx3.py
@@ -1,36 +1,35 @@
 import string
  
 infile = open("C:\Users\NinjaZeroOne\Desktop\httlacs\AliceInWonderland.txt", 'r')
-#outfile = open("alice_words.txt", 'w')
+outfile = open("alice_words.txt", 'w')
  
 aliceDiction = {}           #create empty dictionary
  
 for aline in infile:  
     for aword in aline.split():        
         
-
         # remove punctuation
         aword = aword.replace('_', '').replace('"', '').replace(',', '').replace('.', '')
         aword = aword.replace('-', '').replace('?', '').replace('!', '').replace("'", "")
         aword = aword.replace('(', '').replace(')', '').replace(':', '').replace('[', '')
         aword = aword.replace(']', '').replace(';', '')
-   
         
+        aword = aword.lower()
+
         if aword.isalpha():             #Make sure it is not a number
             if aword in aliceDiction:  #Check to see if word is in dictionary
                 aliceDiction[aword] = aliceDiction[aword] + 1
             else:
                 aliceDiction[aword] = 1
  
-  
-#print(aliceDiction)   
-#keys = aliceDiction.keys()
-#keys.sort()
+    
+keys = aliceDiction.keys()
+keys.sort()
 
-#for aword in keys:
-	#print(str(aword) + "" + str(aliceDiction[aword]))
-    #outfile.write(aword + "" + str(aliceDiction[aword]) )
-    #outfile.write('\n')
+for aword in keys:
+	print(str(aword) + "" + str(aliceDiction[aword]))
+	outfile.write(aword + " " + str(aliceDiction[aword]))
+	outfile.write('\n')
 
-print("The word 'alice' appears " + str(aliceDiction.get('Alice')) + "times in the book.")    
-#outfile.close()
+print("The word 'alice' appears " + str(aliceDiction.get('alice')) + " times in the book.")    
+outfile.close()
