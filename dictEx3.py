f6eda456dded7a192eff5d13d5bea66c1b29cdc5
Anissa Jones <anissajones10@gmail.com>
2015-07-11 21:14:38 -0400

    So this exercise came with an answer. But I wonder if there is another way to take out punctuation. And why are you able to you the .replace one after the other without the object? We weren't taught this which leads me to wonder if there is another way.

--- dictEx3.py
+++ dictEx3.py
@@ -7,7 +7,14 @@ aliceDiction = {}           #create empty dictionary
  
 for aline in infile:  
     for aword in aline.split():        
-       
+        
+
+        # remove punctuation
+        aword = aword.replace('_', '').replace('"', '').replace(',', '').replace('.', '')
+        aword = aword.replace('-', '').replace('?', '').replace('!', '').replace("'", "")
+        aword = aword.replace('(', '').replace(')', '').replace(':', '').replace('[', '')
+        aword = aword.replace(']', '').replace(';', '')
+   
         
         if word.isalpha():             #Make sure it is not a number
             if aword in aliceDiction:  #Check to see if word is in dictionary
