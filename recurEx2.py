92e6054ac4fb345ebf2c3ddff8981b1b3dcdf857
Anissa Jones <anissajones10@gmail.com>
2015-07-13 21:52:41 -0400

    This function works. It returned the reverse of the list entered. But when I asked myself is it a good recursive function and would it work twice in a row, it did not. The second time around, it added the second list to the first list.

--- recurEx2.py
+++ recurEx2.py
@@ -1,8 +1,7 @@
 #Write a recursive function to reverse a list
 
 def lsReverse(numlist, new_list = []):
- 
-    if len(new_list) == len(numlist):
+    if len(numlist) == 0:
         return new_list
     else:
     	new_list.append(numlist[-1])
@@ -10,3 +9,4 @@ def lsReverse(numlist, new_list = []):
     	return lsReverse(numlist)
 
 print(lsReverse([1,3,5,7,9]))
+print(lsReverse([74, 2, 5, 32, 100]))
