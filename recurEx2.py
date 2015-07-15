ca5a16c5a953c6cea7100ab81e78c609998ac99d
Anissa Jones <anissajones10@gmail.com>
2015-07-15 19:20:37 -0400

    I went to a Women Who Code meetup and got help on this one. It was explained to me that I had to make new_list equal None and add an if statement to account for starting with an empty list if new_list was None, and finally return new_list as well so that the program would not think new_list equaled None still. After the first pass of the if statement, new_list would equal the last number in the list, not None.

--- recurEx2.py
+++ recurEx2.py
@@ -1,12 +1,15 @@
 #Write a recursive function to reverse a list
 
-def lsReverse(numlist, new_list = []):
+def lsReverse(numlist, new_list = None):
+    if new_list is None:
+        new_list = []
+
     if len(numlist) == 0:
         return new_list
     else:
-    	new_list.append(numlist[-1])
-    	numlist.pop(-1)
-    	return lsReverse(numlist)
+        new_list.append(numlist[-1])
+        numlist.pop(-1)
+        return lsReverse(numlist, new_list)
 
 print(lsReverse([1,3,5,7,9]))
 print(lsReverse([74, 2, 5, 32, 100]))
